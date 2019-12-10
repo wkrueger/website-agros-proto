22fef4bc4fb9f4bb0b3f2af2238590b7f49a0380
Willian Krueger <wkrueger128@gmail.com>
2019-12-10 17:47:54 -0300

    init

---
+++ components/Main.tsx
@@ -0,0 +1,29 @@
+export function Main() {
+  return (
+    <div>
+      <Nav></Nav>
+    </div>
+  )
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <img src="/public/simfaz-svg-cortado.png" />
+        </li>
+        <li>{/* spacer */}</li>
+        <li>O sistema</li>
+        <li>Diagnóstico</li>
+        <li>Monitoramento</li>
+        <li>Socioambiental</li>
+        <li>A empresa</li>
+        <li>Contato</li>
+        <li>
+          <img src="" />
+        </li>
+      </ul>
+    </nav>
+  )
+}
