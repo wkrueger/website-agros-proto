82d849c860a7ebf248afafe39c5eaa554d31e48b
Willian Krueger <wkrueger128@gmail.com>
2019-12-16 14:33:56 -0300

    rascunho - sticky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    "plugin:react/recommended" // Uses the recommended rules from @eslint-plugin-react
+    'plugin:react/recommended' // Uses the recommended rules from @eslint-plugin-react
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+    sourceType: 'module', // Allows for the use of imports
     ecmaFeatures: {
       jsx: true // Allows for the parsing of JSX
     }
@@ -13,11 +13,12 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    "react/react-in-jsx-scope": "off"
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off'
   },
   settings: {
     react: {
-      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
     }
   }
 }
