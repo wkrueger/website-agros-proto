d2507283d614f6056b1d3990368020857d66a4cf
Willian Krueger <wkrueger128@gmail.com>
2019-12-17 18:50:00 -0300

    mais SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,6 +21,26 @@ class MyDocument extends Document {
             content="Inovação tecnológica a serviço do monitoramento remoto de imóveis e glebas rurais."
           />
 
+          <meta property="og:title" content="SIMFAZ - Sistema de Monitoramento de Fazendas" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://simfaz.com.br" />
+          <meta
+            property="og:image"
+            content="https://simfaz.com.br/public/simfaz-logo-pequeno.svg"
+          />
+          <meta
+            property="og:description"
+            content="Inovação tecnológica a serviço do monitoramento remoto de imóveis e glebas rurais."
+          />
+
+          <meta name="twitter:card" content="summary_large_image"></meta>
+          <meta name="twitter:site" content="@AGROSATELITE"></meta>
+          <meta name="twitter:title" content="SIMFAZ - Sistema de Monitoramento de Fazendas"></meta>
+          <meta
+            name="twitter:description"
+            content="Inovação tecnológica a serviço do monitoramento remoto de imóveis e glebas rurais."
+          ></meta>
+
           {/* icons */}
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
